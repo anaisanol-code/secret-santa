ced50f30baaa2ad3bda9312a06d505cd0030460c
anaisanol-code <anais.anol@laposte.net>
2025-12-20 00:12:31 +0100

    Remove Firebase setup from firebase.js
    Remove Firebase configuration and initialization code.

--- firebase.js
+++ firebase.js
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
-};
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
